f2928eaf6405641e56cfa7fac794d9968b00e38a
Victor Chai <Vic.P.Chai@gmail.com>
2022-11-10 15:46:42 -0500

    Solved and tested up to tempConversion

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...x) {
+  return x[0].filter((el) => !x.includes(el));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (a > b) {
+    let sum = 0;
+    for (let i = b; i < a + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    let sum = 0;
+    for (let i = a; i < b + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
